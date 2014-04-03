c733275cd4d6c41e47b89d44ad32018fcb006f28
curxis114 <symphonia114@hotmail.co.jp>
2014-04-03 22:25:56 +0900

    first commit

---
+++ config.sapmle/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '0237b68602ee26c936facf82b6efff1b1938214ba28f243d2cb5057504f62f1cac9dcea83f712eb74c7320c3bd4eb63b3f7695038a248f55fc49ecf0a2dd313c'
