426f55cc9d9d6af319578678a863a34d75393898
yamamichi <yamamichi@welox.jp>
2014-05-23 15:07:23 +0900

    downの内容を本に記載されているコードに変更

--- db/migrate/20140523044128_combine_items_in_cart.rb
+++ db/migrate/20140523044128_combine_items_in_cart.rb
@@ -17,17 +17,15 @@ class CombineItemsInCart < ActiveRecord::Migration
   end # def 
 
   def down
-    Cart.all.each do |cart|
-      sums = cart.line_items.group(:product_id).sum(:quantity)
-      sums.each do |product_id, quantity|
-        if quantity > 1
-          cart.line_items.where(product_id: product_id).delete_all
-          quantity.times do 
-            cart.line_items.create(product_id: product_id, quantity: 1)
-          end # quantity
-        end # if
-      end # sums
-    end # cart.all
+    # 数量>1の品目を複数の品目に分割する
+    LineItem.where("quantity > 1").each do |line_item|
+      line_item.quantity.times do
+        # 個別の品目を追加する
+        LineItem.create(cart_id: line_item.cart_id, product_id: line_item.product_id, quantity: 1) 
+      end
+      # 元の品目を削除する
+      line_item.destroy
+    end # Line_items.all
   end # def
 
 end
